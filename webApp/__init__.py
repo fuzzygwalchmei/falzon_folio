447eb0bcba46bc73c2dd9cdaf727343e96393ac6
fuzzygwalchmei <marc.falzon@gmail.com>
2021-01-02 19:29:01 +1100

    update for root page

--- webApp/__init__.py
+++ webApp/__init__.py
@@ -15,7 +15,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.environ.get('secret_key')
 
-app.register_blueprint(home_blueprint, url_prefix="/home")
+app.register_blueprint(home_blueprint, url_prefix="/")
 app.register_blueprint(education_blueprint, url_prefix="/education")
 app.register_blueprint(gaming_blueprint, url_prefix="/gaming")
 app.register_blueprint(blog_blueprint, url_prefix="/blog")
